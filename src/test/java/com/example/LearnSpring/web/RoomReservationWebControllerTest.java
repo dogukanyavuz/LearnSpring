4dc6468a97e3b6c3ab3188e1d0ecc6c179ab9339
dogukan.yavuz <dogukany532@gmail.com>
2021-12-02 07:03:32 -0800

    committ

---
+++ src/test/java/com/example/LearnSpring/web/RoomReservationWebControllerTest.java
@@ -0,0 +1,10 @@
+package com.example.LearnSpring.web;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(RoomReservationWebController.class)
+public class RoomReservationWebControllerTest {
+}
